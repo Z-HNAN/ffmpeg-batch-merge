704b779b103542574ca4d6a669498dcc3a0cdfaf
Z-HNAN <zhn68033@163.com>
2020-04-25 10:26:31 +0800

    feat ts版本改写

---
+++ src/Sync.ts
@@ -0,0 +1,22 @@
+import ActionCreator, { Action, ConstructorParams } from './ActionCreator';
+
+// 同步类型的Action
+type SyncAction = {
+  (payload?: any): Action;
+  TYPE: string;
+}
+
+/* Sync actions */
+class Sync<T extends string> extends ActionCreator<T> {
+  constructor(options: ConstructorParams<T>) {
+    super(options);
+
+    this.bindInstanceWithActions();
+  }
+}
+
+export default function syncCreator<T extends string>(
+  config: ConstructorParams<T>,
+): Record<T, SyncAction> {
+  return new Sync(config) as any;
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default as asyncCreator } from './Async';
+export { default as syncCreator } from './Sync';
